c17acfa02c38dfd18ce409c5465c4d66d253f77f
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-23 09:50:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_17__The_For_Loop_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the while loop for reference");
+            int i = 0;
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++ ;
+            }
+
+            Console.WriteLine("The for loop");
+            for(i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        } 
+    
+    }
+}
